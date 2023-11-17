4fb00907fe9213e007073c2c61d3752af6871ffb
Aman Gupta <aman.gupta@iitgn.ac.in>
2023-11-17 16:12:32 +0530

    modifications

--- NetworksLab/congestionTest.py
+++ NetworksLab/congestionTest.py
@@ -1,21 +1,22 @@
 from mininet.topo import Topo
 from mininet.net import Mininet
-from mininet.node import CPULimitedHost
-from mininet.link import TCLink
+from mininet.node import Node
+from mininet.log import setLogLevel, info
 from mininet.cli import CLI
+import sys
 import time
-class NetworkTopo(Topo):
-    "A simple network topology with two switches and four hosts"
 
+class NetworkTopo(Topo):
     def build(self, **_opts):
+        
         s1 = self.addSwitch('s1')
         s2 = self.addSwitch('s2')
 
-        h1 = self.addHost('h1', ip='10.0.0.1')
-        h2 = self.addHost('h2', ip='10.0.0.2')
-        h3 = self.addHost('h3', ip='10.0.0.3')
-        h4 = self.addHost('h4', ip='10.0.0.4')
-
+        h1 = self.addSwitch('h1')
+        h2 = self.addSwitch('h2')
+        h3 = self.addSwitch('h3')
+        h4 = self.addSwitch('h4')
+        
         self.addLink(s1, s2, intfName1='s1-eth0', intfName2='s2-eth0')
         self.addLink(h1, s1, intfName2='s1-eth1')
         self.addLink(h2, s1, intfName2='s1-eth2')
@@ -23,24 +24,46 @@ class NetworkTopo(Topo):
         self.addLink(h4, s2, intfName2='s2-eth2')
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Your script description')
+
+    parser.add_argument('--config', choices=['b', 'c'], required=True,
+                        help='Specify config option: b or c')
+    parser.add_argument('--scheme', choices=['reno', 'vegas', 'cubic', 'bbr', 'all'], required=False,
+                        help='Specify scheme option: reno, vegas, cubic, bbr, or all')
+    parser.add_argument('--loss', type=float, required=False,
+                        help='Specify loss as a number')
+
+    args = parser.parse_args()
+    configValue = args.config
+    schemeValue = args.scheme
+    lossValue = args.loss
+    
+
+    schemeAsArg = ""
+    if schemeValue:
+        schemeAsArg = " -Z " + schemeValue
+
+    #start h4 in server mode no matter what
+    h1, h2, h3, h4 = net.getNodeByName('h1', 'h2', 'h3', 'h4')
+    h4Server = h4.popen(['iperf', '-s'])
+
+    
+    if configValue == 'b':
+        h1Client = h1.popen(f'iperf -c {h4.IP()}{schemeAsArg}', shell=True)
+        h1Client.wait()
+    else:
+        h1Client = h1.popen(f'iperf -c {h4.IP()}{schemeAsArg}', shell=True)
+        h2Client = h2.popen(f'iperf -c {h4.IP()}{schemeAsArg}', shell=True)
+        h3Client = h3.popen(f'iperf -c {h4.IP()}{schemeAsArg}', shell=True)
+        h1Client.wait()
+        h2Client.wait()
+        h3Client.wait()
+
+    
     topo = NetworkTopo()
-    net = Mininet(topo=topo, controller=None)
+    net = Mininet(topo=topo, waitConnected=True)
     net.start()
 
-    # Add a small delay before attempting to ping
-    time.sleep(2)
-
-    # Now let's ping between hosts
-    h1, h2, h3, h4 = net.get('h1', 'h2', 'h3', 'h4')
-
-    # Ping h2 from h1
-    result1 = h1.cmd('ping -c 3', h2.IP())
-    print(f'Ping result from h1 to h2:\n{result1}')
-
-    # Ping h3 from h4
-    result2 = h4.cmd('ping -c 3', h3.IP())
-    print(f'Ping result from h4 to h3:\n{result2}')
-
-    # Stop the Mininet network
+    CLI(net)
+    h4Server.terminate()
     net.stop()
-
